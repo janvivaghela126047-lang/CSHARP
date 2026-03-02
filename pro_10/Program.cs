b50195d9a27e23261c3645a90afe16b38b0eba76
janvivaghela126047-lang <janvi.vaghela126047@marwadiuniversity.ac.in>
2026-03-02 21:58:11 +0530

    Add files via upload

---
+++ pro_10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("eneter number :");
+            int n = int.Parse(Console.ReadLine());
+            int i, m;
+            int flag = 0;
+            for (i = 2; i < n - 1; i++)
+            {
+                m = n % 1;
+                if(m==0)
+                {
+                    Console.WriteLine("not prime number ");
+                    flag = 1;
+                    break;
+                }
+
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("prime number ");
+            }
+
+        }
+    }
+}
---
+++ pro_9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ente number: ");
+            int n = int.Parse(Console.ReadLine());
+            int n1 = 0;
+            int n2 = 1;
+            int n3 = 1;
+            Console.Write(n1 + " ");
+            Console.Write(n2 + " ");
+            while (n3 <= n)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
