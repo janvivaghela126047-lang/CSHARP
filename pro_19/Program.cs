a50bd64105413d54a8fa1be8892ee99062b4cab0
janvivaghela126047-lang <janvi.vaghela126047@marwadiuniversity.ac.in>
2026-03-30 19:02:39 +0530

    Add files via upload

---
+++ pro_19/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+            int max;
+
+            Console.WriteLine("enter 5 number: ");
+            for (int i = 1; i < 5; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            max = a[0];
+            for (int i = 1; i < 5; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+            Console.WriteLine("maximum number is : " + max);
+        }
+    }
+}
