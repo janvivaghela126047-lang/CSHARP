2bce2eb4636da9380967a5722aeb38c534d6b734
janvivaghela126047-lang <janvi.vaghela126047@marwadiuniversity.ac.in>
2026-03-30 19:01:45 +0530

    Add files via upload

---
+++ pro_13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, min;
+
+            Console.WriteLine("enter first number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter third number: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            min = (a < b) ?
+                ((a < c) ? a : c)
+                :
+                ((b < c) ? b : c);
+            Console.WriteLine("Minimum number is :" +  min);
+
+                
+        }
+    }
+}
---
+++ pro_14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number:");
+            int n = int.Parse(Console.ReadLine());
+            int m, rev = 0;
+            int temp = n;
+            while (n > 0)
+            {
+                m = n % 10;
+                rev = rev * 10 + m;
+                n = n / 10;
+            }
+            if (rev == temp)
+            {
+                Console.WriteLine("number is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("number is not palindrom");
+            }
+        }
+    }
+}
