38128013fafdf5199403c98a5337e1214db70d8e
janvivaghela126047-lang <janvi.vaghela126047@marwadiuniversity.ac.in>
2026-04-07 19:44:58 +0530

    Add files via upload

---
+++ pro_24/Triangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro_24
+{
+    internal class Triangle:Shape
+    {
+        private double basevalue;
+        private double height;
+
+        public Triangle(double b, double h)
+        {
+            basevalue = b;
+            height = h;
+        }
+
+        public override double Area()
+        {
+            return basevalue * height;
+        }
+    }
+}
