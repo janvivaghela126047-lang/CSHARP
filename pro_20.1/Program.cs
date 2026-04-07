0639b8a2dd7d2cce87c586fbae6a7110dce7f1ad
janvivaghela126047-lang <janvi.vaghela126047@marwadiuniversity.ac.in>
2026-04-07 18:03:05 +0530

    Add files via upload

---
+++ pro_20.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro_20._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Rectangle r1 = new Rectangle();
+            r1.calculate();
+            r1.display();
+            Rectangle r2 = new Rectangle(30, 40);
+            r2.calculate();
+            r2.display();
+        }
+    }
+}
